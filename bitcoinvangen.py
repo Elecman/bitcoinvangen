ad2bad2c3f1581a8d1ad7d1145ea118ac64de9de
Artur Nunes <33733651+Keepler@users.noreply.github.com>
2018-12-30 07:29:24 -0200

    Update bitcoinvangen.py
    Major bugs fixed

--- bitcoinvangen.py
+++ bitcoinvangen.py
@@ -1,64 +1,64 @@
 from hashlib import sha256
-from numpy.random import bytes
-from multiprocessing import Process
-from time import time
+from numpy.random import bytes, seed
+from bitcoin import *
+from multiprocessing import Process, Value
+from ctypes import c_bool
+from time import time, sleep
 import sys
-import bitcoin
 
-def generate(thread):
+def startGenerating(name, caseSens, thread, running):
     timestamp = int(time())
     rate = times = 0
 
     while True:
+        if not running.value:
+            sys.exit()
+
         if int(time()) == timestamp + 1:
             timestamp += 1
             if times < 10: times += 0 
             if times == 10: print(f'Thread {thread} is generating ~{rate} wallets per second.')
+            times += 1
             rate = 0
 
         rate += 1
 
         random_bytes = bytes(32)
+        privkey = sha256(random_bytes)
+        pubkey = privtopub(privkey)
+        address = pubtoaddr(pubkey)
 
-        privkey = sha256(random_bytes).hexdigest()
-        pubkey = bitcoin.privtopub(privkey)
-        address = bitcoin.pubtoaddr(pubkey)
-
-        if not caseSens and address[1:len(name) + 1].lower() == name.lower():
-            walletData = f'''Address: {address}\nPrivate key: {privkey}'''
-
-            with open('wallet.txt', 'w+') as file:
-                file.write(walletData)
-                file.close()
-
-            print('Your vanity wallet was generated and was saved to wallet.txt.')
-            sys.exit()
-
-        if caseSens and address[1:len(name) + 1] == name:
-            walletData = f'Address: {address}\nPrivate key: {privkey}'
-
-            with open('wallet.txt', 'w+') as file:
-                file.write(walletData)
-                file.close()
+        if (not caseSens and address[1:len(name) + 1].lower() == name.lower()) or (caseSens and address[1:len(name) + 1] == name):
+            running.value = False
+            print('-'*100)
+            print(f'''\nWallet successfully generated!\n\nAddress: {address}\nPrivate key: {privkey}\n''')
+            print('-'*100)
+            print('\nThe private key gives total access to your coins, so NEVER share it with anyone.')
+            print("Store the private key in a safe place IMMEDIATELY.\n\nThanks for using keepler's VanGen.\nYou can contribute to this project at github.com/Keepler/bitcoinvangen")
             
-            print('Your vanity wallet was generated and was saved to wallet.txt.')
-            sys.exit()
-
 if __name__ == '__main__':
+    print("\nWelcome to keepler's Bitcoin VanGen\n\nThe name can't contain maiuscle O, minuscle L, maiuscle i or zero, symbols or more than 32 characters.")
 
     while True:
-        print("The name can't contain maiuscle O, minuscle L, maiuscle i or zero, nor be more than 32 characters long.")
         name = str(input('Input a name (the longer the name be, more time it will take): '))
+        valid = True
 
-        if 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789' not in name and len(name) > 32:
-            print('Invalid name.')
-        else:
-            break
+        for x in name:
+            if x not in 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789' or len(name) > 32:
+                valid = False
+                print('Invalid name.')
+            else:
+                break
+                
+        if valid: break
 
     while True:
         caseSens = str(input('Case sensitive? (takes longer) (y/n) '))
-
-        if caseSens.lower() == 'y' or caseSens.lower() == 'n':
+        if caseSens.lower() == 'y':
+            caseSens = True
+            break
+        elif caseSens.lower() == 'n':
+            caseSens = False
             break
         else:
             print('Your answer must be "y" or "n".')
@@ -70,11 +70,12 @@ if __name__ == '__main__':
         except:
             print('It must be a number.')
 
-    print('Generating...\nIt may take a long time.')
-
+    print("\nIt's highly recommended turning your internet connection off during this process.\nGenerating, it may take a long time...\n")
+    
+    running = Value(c_bool, True)
     processes = []
     for x in range(0, threads):
-        processes.append(Process(target=generate, args=(x,)))
-
+        processes.append(Process(target=startGenerating, args=(name, caseSens, x, running,)))
+        
     for x in processes:
         x.start()
